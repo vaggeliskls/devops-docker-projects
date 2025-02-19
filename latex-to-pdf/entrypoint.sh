0e188d036d66467572ba5ac5b5d8fba1d71ab6df
vaggeliskls <vaggeliskls@gmail.com>
2025-02-19 16:35:18 +0200

    feat: use custom interval for better support

--- latex-to-pdf/entrypoint.sh
+++ latex-to-pdf/entrypoint.sh
@@ -6,9 +6,23 @@ if [ ! -f "$TEX_FILE" ]; then
 fi
 
 pdflatex "$TEX_FILE" || { echo "Initial compilation failed" >&2; exit 1; }
+echo "-> Initial compilation successful"
+
+# we use interval due to windows issue with inotify and fwatch
+# Set sleep interval from env or default to 2 seconds
+INTERVAL=${INTERVAL:-2}
 
 if [ "$WATCH" = "true" ]; then 
-    while inotifywait -e modify "$TEX_FILE"; do 
-        pdflatex "$TEX_FILE" || echo "Recompilation failed" >&2
+    echo "-> Watching for changes in $TEX_FILE (Polling every $SLEEP_INTERVAL seconds)"
+    LAST_MOD_TIME=$(stat -c %Y "$TEX_FILE")
+    while true; do
+        sleep "$INTERVAL"
+        NEW_MOD_TIME=$(stat -c %Y "$TEX_FILE")
+
+        if [ "$NEW_MOD_TIME" != "$LAST_MOD_TIME" ]; then
+            echo "-> File changed, recompiling"
+            pdflatex "$TEX_FILE" || echo "Recompilation failed" >&2
+            LAST_MOD_TIME=$NEW_MOD_TIME
+        fi
     done
 fi
